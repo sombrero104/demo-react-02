f155b2f5ffed6ae8099f1e36230e87578edeef41
sombrero104 <recipe1@naver.com>
2026-02-25 21:24:00 +0900

    .

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,8 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import NotFound from './pages/NotFound';
 import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
+import Header from './components/Header';
 
 function App() {
     const nav = useNavigate();
@@ -15,7 +17,7 @@ function App() {
 
     return (
         <>
-            <div>
+            {/* <div>
                 <img src={getEmotionImage(1)} />
                 <img src={getEmotionImage(2)} />
                 <img src={getEmotionImage(3)} />
@@ -27,7 +29,34 @@ function App() {
                 <Link to={"/new"}>New</Link>
                 <Link to={"/diary"}>Diary</Link>
             </div>
-            <button onClick={onClickButton}>New 페이지로 이동</button>
+            <button onClick={onClickButton}>New 페이지로 이동</button> */}
+
+            <Header
+                title={"Header"}
+                leftChild={<Button text={"Left"} />}
+                rightChild={<Button text={"Right"} />}
+            />
+            <Button
+                text={"123"}
+                type={"DEFAULT"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭.");
+                }}
+            />
+            <Button
+                text={"123"}
+                type={"POSITIVE"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭.");
+                }}
+            />
+            <Button
+                text={"123"}
+                type={"NEGATIVE"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭.");
+                }}
+            />
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/new" element={<New/>} />
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
     const [params, setParams] = useSearchParams();
-    console.log("# value: ", params.get("value"));  // 쿼리스트링 파라미터. (http://localhost:5173/?value=hi)
+    // console.log("# value: ", params.get("value"));  // 쿼리스트링 파라미터. (http://localhost:5173/?value=hi)
 
     return <div>Home</div>;
 };
